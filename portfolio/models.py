35fd71ef528c132dc4e0ada9b8e2b3af75018dec
candizao <161033998+candizao@users.noreply.github.com>
2026-04-10 22:43:30 +0100

    Update models.py

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Licenciatura(models.Model):
+    nome = models.CharField(max_length=100, default="Informatica de Gestao")
+    ano_inicio = models.IntegerField(default=2024)
+    ano_previsto_fim = models.IntegerField(default=2027)
+    instituicao = models.CharField(max_length=100, default="Universidade Lusófona")
+
+    def __str__(self):
+        return self.nome
